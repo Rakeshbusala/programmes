3d3e6de64ad8d946914efda1a5c15de3794f2d59
Rakeshbusala <99265494+Rakeshbusala@users.noreply.github.com>
2022-02-10 18:41:02 +0530

    Create Assignment 1(10-02-2022).py

---
+++ Assignment 1(10-02-2022).py
@@ -0,0 +1,11 @@
+#PROGRAM FOR STUDENT RESULTS
+Stu_no=int(input("Enter the student no : "))
+Stu_name=input("Enter the student name : "))
+Stu_sec=input("Enter the student group : "))
+Sub1=int(input("Enter the computers(1) marks : "))
+Sub2=int(input("Enter the computers(2) marks : "))
+Sub3=int(input("Enter the computers(3) marks : "))
+if sub1>=35 and sub2>=35 and sub3>=35:
+ print(Stu_name,"-----PASS.")
+else:
+ print(Stu_nmae,"-----FAIL.")
